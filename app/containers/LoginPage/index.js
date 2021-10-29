18561ddaf5dfb97ebc5c11fd8bb8e00ae69a9b68
gaurav sharma <gsharma774962@gmail.com>
2021-10-29 19:33:37 +0530

    pushing login page

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -1,9 +1,19 @@
-import { Grid } from '@material-ui/core';
+import { Button, Checkbox, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { useStyles } from './styles';
+import LoginPageSVG from '../../images/LoginPageSVG';
+import Logo from '../../images/Logo';
 
 const LoginPage = () => {
   const classes = useStyles();
+  const [mailFocus, setMailFocus] = React.useState('none');
+  const setFocused = name => {
+    if (name === 'mail') {
+      setMailFocus('mail');
+    } else {
+      setMailFocus('name');
+    }
+  };
   return (
     <Grid
       container
@@ -11,8 +21,97 @@ const LoginPage = () => {
       className={classes.root}
       justifyContent="center"
     >
-      <Grid direction="column" className={classes.left} />
-      <Grid direction="column" className={classes.right} />
+      <Grid direction="column" className={classes.left}>
+        <div className={classes.logoBox}>
+          <Logo className={classes.logo} />
+        </div>
+        <div className={classes.inputFieldsContainer}>
+          <Typography className={classes.welcomeText} variant="h1">
+            welcome back
+          </Typography>
+          <Typography variant="h6" className={classes.welcomeMessage}>
+            Thank you for getting back to us,please login to your account by
+            filling this form:
+          </Typography>
+          <Grid direction="column">
+            <div
+              className={
+                mailFocus === 'mail'
+                  ? classes.inputWrapFocused
+                  : classes.inputWrap
+              }
+            >
+              <Typography variant="body1" className={classes.email}>
+                Email Address
+              </Typography>
+              <input
+                onFocus={() => setFocused('mail')}
+                className={classes.inputFieldEmail}
+              />
+            </div>
+            <div
+              className={
+                mailFocus === 'name'
+                  ? classes.inputWrapFocusedName
+                  : classes.inputWrapName
+              }
+            >
+              <Typography variant="body1" className={classes.email}>
+                Password
+              </Typography>
+              <input
+                type="password"
+                autoComplete="new-password"
+                required
+                onFocus={() => setFocused('name')}
+                className={classes.inputFieldEmail}
+              />
+            </div>
+            <Grid
+              className={classes.checkBoxContainer}
+              container
+              direction="row"
+            >
+              <Checkbox
+                defaultChecked
+                classes={{ root: classes.checkBox }}
+                size="medium"
+              />
+              <Typography className={classes.checkBoxLabel} variant="body1">
+                Remember me
+              </Typography>
+              <a href="*">
+                <Typography className={classes.forgotPassword} variant="body1">
+                  Forgot Passowrd
+                </Typography>
+              </a>
+            </Grid>
+            <Grid
+              direction="row"
+              container
+              className={classes.ButtonContainer}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <>
+                <Button color="secondary" className={classes.loginButton}>
+                  LOGIN
+                </Button>
+                <Button className={classes.signUpButton}>SIGN UP</Button>
+              </>
+            </Grid>
+          </Grid>
+        </div>
+      </Grid>
+      <Grid
+        direction="column"
+        className={classes.right}
+        container
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        <LoginPageSVG className={classes.LoginPageSVG} />
+      </Grid>
     </Grid>
   );
 };
--- app/containers/LoginPage/styles.js
+++ app/containers/LoginPage/styles.js
@@ -1,14 +1,134 @@
 import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(() => ({
   root: {
-    width: '100%',
+    width: '100vw',
     height: '100vh',
   },
   left: {
-    backgroundColor: 'red',
-    flexGrow: 1,
+    width: '40%',
   },
   right: {
-    flexGrow: 1,
+    width: '60%',
+  },
+  LoginPageSVG: {
+    transform: 'scale(0.55)',
+  },
+  logoBox: {
+    height: '80px',
+    width: '400px',
+    overflow: 'hidden',
+    position: 'relative',
+    top: '40px',
+    left: '100px',
+    borderLeft: '30px solid hsla(228, 100%, 69%, 1)',
+  },
+  logo: {
+    position: 'absolute',
+    top: '-20px',
+  },
+  inputFieldsContainer: {
+    margin: '120px 0 0 100px',
+  },
+  welcomeText: {
+    color: ' hsla(228, 100%, 69%, 1)',
+    fontWeight: 600,
+  },
+  welcomeMessage: {
+    fontFamily: "'Montserrat'",
+    width: '80%',
+    color: '#B8B8B8',
+    fontWeight: 200,
+    marginBottom: '30px',
+  },
+  inputFieldEmail: {
+    width: '100%',
+    height: '40%',
+    paddingLeft: '10px',
+    border: 0,
+    fontSize: '1.5rem',
+    color: 'black',
+    backgroundColor: 'transparent',
+    '&:focus': {
+      outline: 'none',
+    },
+  },
+  inputWrap: {
+    width: '90%',
+    height: '7vh',
+    border: '1px solid #B8B8B8',
+    borderLeft: '8px solid  #B8B8B8',
+  },
+  inputWrapFocused: {
+    width: '90%',
+    height: '7vh',
+    border: '1px solid #B8B8B8',
+    borderLeft: '8px solid #61a5c2',
+  },
+  inputWrapName: {
+    width: '90%',
+    height: '7vh',
+    border: '1px solid #B8B8B8',
+    borderLeft: '8px solid  #B8B8B8',
+    borderTop: 0,
+  },
+  inputWrapFocusedName: {
+    width: '90%',
+    height: '7vh',
+    border: '1px solid #B8B8B8',
+    borderLeft: '8px solid #61a5c2',
+    borderTop: 0,
+  },
+  email: {
+    fontFamily: "'Montserrat'",
+    fontWeight: 700,
+    color: '#B8B8B8',
+    marginLeft: '10px',
+  },
+  checkBoxContainer: {
+    marginTop: '4vh',
+    position: 'relative',
+  },
+  checkBoxLabel: {
+    color: '#B8B8B8',
+    fontFamily: "'Montserrat'",
+  },
+  checkBox: {
+    padding: 0,
+    left: '-3px',
+    '&>.MuiIconButton-label': {
+      color: 'hsla(228, 100%, 69%, 1)',
+    },
+  },
+  forgotPassword: {
+    position: 'absolute',
+    right: 65,
+    fontFamily: "'Montserrat'",
+    fontWeight: 700,
+  },
+  ButtonContainer: {
+    marginTop: '100px',
+    width: '90%',
+  },
+  loginButton: {
+    width: '150px',
+    height: '50px',
+    backgroundColor: '#1597E5',
+    color: 'white',
+    fontSize: '1.2rem',
+    borderRadius: 0,
+    marginRight: '30px',
+    '&:hover': {
+      backgroundColor: '#D1E8E4',
+    },
+  },
+
+  signUpButton: {
+    width: '150px',
+    height: '50px',
+    backgroundColor: 'transparent',
+    color: 'black',
+    border: '1px solid grey',
+    borderRadius: 0,
+    fontSize: '1.2rem',
   },
 }));
