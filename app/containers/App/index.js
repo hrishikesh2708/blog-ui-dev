4352837eed8f97482de2ec5a5db191279b087068
gaurav sharma <gsharma774962@gmail.com>
2021-10-29 11:03:52 +0530

    removing container and components

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,14 +9,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
-
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
@@ -37,14 +29,14 @@ export default function App() {
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
       <GlobalStyle />
+      <div>
+        <h3>
+          Container App index.js * * This component is the skeleton around the
+          actual pages, and should only * contain code that should be seen on
+          all pages. (e.g. navigation bar)
+        </h3>
+      </div>
     </AppWrapper>
   );
 }
